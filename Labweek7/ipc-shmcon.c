103cb5d218a41af8abe1e71d5cd55e38f15c2903
StuGeek <978475534@qq.com>
2021-04-07 21:00:35 +0800

    3.5

--- Labweek7/ipc-shmcon.c
+++ Labweek7/ipc-shmcon.c
@@ -16,11 +16,11 @@
 #define OP_MODIFY 4
 
 char names[30][30] = {"name1", "name2", "name3", "name4", "name5", 
-                    "name6", "name7", "name8", "name9", "name10",
-                        "name11", "name12", "name13", "name14", "name15", 
-                            "name16", "name17", "name18", "name19", "name20",
-                                "name21", "name22", "name23", "name24", "name25", 
-                                    "name26", "name27", "name28", "name29", "name30"};
+                        "name6", "name7", "name8", "name9", "name10",
+                            "name11", "name12", "name13", "name14", "name15", 
+                                "name16", "name17", "name18", "name19", "name20",
+                                    "name21", "name22", "name23", "name24", "name25", 
+                                        "name26", "name27", "name28", "name29", "name30"};
 
 //根据op的值选择操作种类，1为插入，2为删除，3为查找，4为修改
 void operation(MinHeap heap, int op) {
