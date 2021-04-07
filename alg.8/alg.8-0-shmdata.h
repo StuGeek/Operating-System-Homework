6f51a17642c9e8b66ab8db4c321b614a9dab98c4
StuGeek <978475534@qq.com>
2021-04-07 21:08:41 +0800

    2.9

--- alg.8/alg.8-0-shmdata.h
+++ alg.8/alg.8-0-shmdata.h
@@ -1,17 +1,12 @@
 #define TEXT_SIZE 4*1024  /* = PAGE_SIZE, size of each message */
-#define TEXT_NUM 20      /* maximal number of mesages */
+#define TEXT_NUM 1      /* maximal number of mesages */
     /* total size can not exceed current shmmax,
        or an 'invalid argument' error occurs when shmget */
 
-typedef struct {
-    char text[TEXT_SIZE];
-} Queue;
-
 /* a demo structure, modified as needed */
 struct shared_struct {
-    Queue mtext[TEXT_NUM + 1]; /* buffer for message reading and writing */
-    int front; /*队首元素的下标*/
-    int rear; /*队尾元素的下标*/
+    int written; /* flag = 0: buffer writable; others: readable */
+    char mtext[TEXT_SIZE]; /* buffer for message reading and writing */
 };
 
 #define PERM S_IRUSR|S_IWUSR|IPC_CREAT
